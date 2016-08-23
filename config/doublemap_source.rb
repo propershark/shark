27ded780c6046377298132756841272e5ff4521f
Jon <audiobahn404@gmail.com>
2016-08-23 15:22:47 -0400

    Include `WLSC` in the stop_code regex. While doing some work on recording objects, I saw that a vehicle had a `next_station` with the identifier "stations.WLSC81". The DoubleMapSource regex for finding `stop_code`s didn't include that possibility. This commit adds WLSC to that regex.

--- config/doublemap_source.rb
+++ config/doublemap_source.rb
@@ -7,7 +7,7 @@ DoubleMapSource.configure do |doublemap|
   # DoubleMap does not have :stop_code as part of it's schema, but it is useful
   # for creating shorter channel names. This lambda essentially patches
   # stop_code onto DoubleMap::Station objects.
-  doublemap.station_key = ->(station){ station.name[/BUS\w*|TEMP\w*/].chomp }
+  doublemap.station_key = ->(station){ station.name[/BUS\w*|TEMP\w*|WLSC\w*/].chomp }
   doublemap.vehicle_key = :name
 
 
