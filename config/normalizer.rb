f7b8182d8c8c68270138fb0c5ce3df6cd3e775ea
Jon <audiobahn404@gmail.com>
2016-09-21 15:29:45 -0400

    Use `sub` instead of `sub!` in Normalizer. This was an interesting find: The same memory for station names (and likely many other attributes) was being used through the entire stack (from the Source, to Agency, and up the Middleware stack). This means that when `Normalizer` did an in-place `sub!` on those strings, the changes cascaded all the way backwards through to the reference kept by the Source instance. Then, when the Source went through its update cycle (which seems to be having caching issues; likely what made this an issue in the first place. See #29), it would have the already-Normalized strings, causing some parse errors where certain formats are expected (e.g., the station name includes it's `stop_code`, which Normalizer removes).
    This commit simply changes `sub!` to `sub` to avoid this cascading effect entirely, and resets some previous changes to `config/doublemap_source.rb` from attempting to deal with this earlier.

--- config/normalizer.rb
+++ config/normalizer.rb
@@ -4,13 +4,13 @@ Normalizer.configure do |config|
   # Remove Station `stop_code`s from their names (they are extracted into
   # `Station#stop_code` at the Source level).
   config.on_shark_station = Proc.new do |station|
-    station.name.sub!(/\s*\W*\s*(BUS|TEMP)\d+\s*$/, '')
+    station.name = station.name.sub(/\s*\W*\s*(BUS|TEMP)\d+\s*$/, '')
   end
 
   # Remove any miscellaneous information from Route names. In particular,
   # schedule information will be sourced from the Timetable server, so having
   # such information in the Route name will be misleading, if nothing else.
   config.on_shark_route = Proc.new do |route|
-    route.name.sub!(/\(Begins[^\)]+\)/, '')
+    route.name = route.name.sub(/\(Begins[^\)]+\)/, '')
   end
 end
